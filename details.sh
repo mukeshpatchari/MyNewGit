d019bdcc876605055ac928228c6fefb0b2b37976
testuser1 <testuser1@ip-10-1-1-100.ec2.internal>
2023-04-26 09:11:54 +0000

    Added sudo code and users

--- details.sh
+++ details.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "THIS SCRIPT WILL PROVIDE INFORMATION ABOUT THE LOCAL SYSTEM"
+#Gives Hostname
 echo $(hostname)
+#Gives User executed
 echo $(whoami)
+#gives cpu info
 echo $(uptime)
-echo $(ll)
+#Gives List of new users
+echo $(cat /etc/passwd | cut -d ":" -f 1 | tail -n 2)
